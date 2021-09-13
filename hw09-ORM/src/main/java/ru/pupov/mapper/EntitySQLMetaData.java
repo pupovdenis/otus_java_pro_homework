3fafb8c1fcb2599acaf50b28159c0283a1481f5f
Пупов Денис Михайлович <dpupov@nvg.ru>
2021-09-13 21:57:17 +0500

    ninth hw

---
+++ hw09-ORM/src/main/java/ru/pupov/mapper/EntitySQLMetaData.java
@@ -0,0 +1,16 @@
+package ru.pupov.mapper;
+
+/**
+ * Создает SQL - запросы
+ */
+public interface EntitySQLMetaData {
+    String getSelectAllSql();
+
+    String getSelectByIdSql();
+
+    String getInsertSql();
+
+    String getUpdateSql();
+
+    EntityClassMetaData getEntityClassMetaData();
+}
